9b2e6426c83e1b915f7d8095170a5c306b10f023
Mehmil Zeeshan <160164164+Fatima2209zIT@users.noreply.github.com>
2025-01-25 12:40:15 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,8 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
-
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+const client = createClient({
+  projectId: "rvjgczr8",
+  dataset: "production",
+  apiVersion: "2025-01-13", // Use the latest API version
+  useCdn: true, // Set to true in production
+});
